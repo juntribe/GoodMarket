a596a80445996e51e355cd786a87360c0975a8cb
juntribe <63856867+juntribe@users.noreply.github.com>
2021-10-13 15:25:42 +0900

    Initial commit

---
+++ src/main/java/com/goodmarket/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.goodmarket;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GoodMarketApplication.class);
+    }
+
+}
---
+++ src/test/java/com/goodmarket/GoodMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
